efe11aa24cc74394fed3f72cb82a64b1d4bdd811
Ratnangi Nirek <ratnangi.nirek@dell.com>
2017-10-30 09:41:32 -0500

    Update oldbranches.py

--- oldbranches.py
+++ oldbranches.py
@@ -4,5 +4,5 @@
 BRANCHES=`git branch -r --merged | grep -v "*" | grep -v master | grep -v develop | grep -v release/ | grep -v hotfix/ | sed 's/origin\///'`
 printf "Removing branches: \n$BRANCHES"
 
-# delete all remote branches merged into the current branch (i.e. develop), excluding release/master/develop branches
-git branch -r --merged | grep -v "*" | grep -v master | grep -v develop | grep -v release/ | grep -v hotfix/ | sed 's/origin\///' | xargs -n 1 git push --delete origin
+# delete all remote branches merged into the current branch (i.e. develop), excluding release/master/stable branches
+git branch -r --merged | grep -v "*" | grep -v master | grep -v stable | grep -v release/ | grep -v hotfix/ | sed 's/origin\///' | xargs -n 1 git push --delete origin
